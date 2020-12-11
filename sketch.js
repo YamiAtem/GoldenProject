98332155426124d318ae761b2278c3836e6e327c
Atharva Mishra <atharvamishra956@gmail.com>
2020-12-11 20:24:28 +0530

    added fire over houses in scene 2

--- sketch.js
+++ sketch.js
@@ -9,10 +9,17 @@ var invisGround, edges;
 
 // character
 var apollo, left, right, lookingLeft, lookingRight, leftSteps, rightSteps, moving, idleLeft, idleRight, idle;
+
+// fire 
+var fire1, fire2, fire, fireAnim;
+
 leftSteps = 0;
 rightSteps = 0;
+
 left = [];
 right = [];
+fire = [];
+
 moving = false;
 lookingLeft = false;
 lookingRight = false;
@@ -30,6 +37,16 @@ function preload() {
         right[i] = loadImage("Apollo/Right/tile07" + i + ".png");
     }
 
+    for (var i = 0; i < 60; i++) {
+        fire[i] = loadImage("NPC/Fire/tile0" + i + ".png")
+    }
+
+     fireAnim = loadAnimation(fire[0], fire[1], fire[2], fire[3], fire[4], fire[5], fire[6], fire[7], fire[8], fire[9], fire[10],
+        fire[11], fire[12], fire[13], fire[14], fire[15], fire[16], fire[17], fire[18], fire[19], fire[20], fire[21], fire[22], 
+        fire[23], fire[24], fire[25], fire[26], fire[27], fire[28], fire[29], fire[30], fire[31], fire[32], fire[33], fire[34],
+        fire[35], fire[36], fire[37], fire[38], fire[39], fire[40], fire[41], fire[42], fire[43], fire[44],fire[45], fire[46], 
+        fire[47], fire[48], fire[49], fire[50], fire[51], fire[52], fire[53], fire[54], fire[55], fire[56], fire[57], fire[58], fire[59])
+
     idleLeft = loadImage('Apollo/Idle/tile010.png');
     idleRight = loadImage('Apollo/Idle/tile030.png');
     idle = loadImage('Apollo/Idle/tile000.png')
@@ -43,49 +60,26 @@ function setup() {
     invisGround= createSprite(windowWidth/2, windowHeight-100, windowWidth, 20);
     invisGround.visible = false;
 
+    fire1 = createSprite(windowWidth/1.75, windowHeight/1.75, 20, 20)
+    fire1.addAnimation('Fire', fireAnim)
+    fire1.visible = false;
+
+    fire2 = createSprite(windowWidth/3.9, windowHeight/1.75, 20, 20)
+    fire2.addAnimation('Fire', fireAnim)
+    fire2.visible = false;
+
+
     edges = createEdgeSprites();
 }
 
 
 function draw() {
-
-    if (scene === '1'){
-        background(scene1)
-    } else if (scene === '2') {
-        background(scene2)
-    } else if (scene ==='3') {
-        background(scene3)
-    }
+    SceneChange();
 
     apollo.collide(invisGround)
 
     moveLeft();
     moveRight();
 
-    // not moving
-    if (moving === false) {
-        if (lookingLeft === true) {
-            apollo.addImage(idleLeft);
-        } else if (lookingRight === true) {
-            apollo.addImage(idleRight);
-        } else {
-            apollo.addImage(idle);
-        }
-    }
-    
-    // cave and villiage scene
-    if (apollo.isTouching(edges[0]) && scene === '1') {
-        console.log("Village on left")
-        scene = '2'
-        apollo.x = windowWidth/3;
-    } else if (apollo.isTouching(edges[0]) && scene === '2') {
-        scene = '1'
-        apollo.x = windowWidth/3;
-        console.log('cave on the right')
-    } else if (apollo.isTouching(edges[1]) && scene === '1') {
-        scene = '3'
-        apollo.x = windowWidth/4;
-        console.log('MoreCave')
-    }
     drawSprites();
 }
