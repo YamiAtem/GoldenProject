185eb030848fe9325fa9ced87305b8c31a236e0b
Atharva Mishra <atharvamishra956@gmail.com>
2020-12-12 19:03:58 +0530

    Added new fire animation

--- sketch.js
+++ sketch.js
@@ -37,19 +37,15 @@ function preload() {
         right[i] = loadImage("Apollo/Right/tile07" + i + ".png");
     }
 
-    for (var i = 0; i < 60; i++) {
-        fire[i] = loadImage("NPC/Fire/tile0" + i + ".png")
+    for (var i = 0; i < 6; i++) {
+        fire[i] = loadImage("NPC/Fire/tile00" + i + ".png")
     }
 
-     fireAnim = loadAnimation(fire[0], fire[1], fire[2], fire[3], fire[4], fire[5], fire[6], fire[7], fire[8], fire[9], fire[10],
-        fire[11], fire[12], fire[13], fire[14], fire[15], fire[16], fire[17], fire[18], fire[19], fire[20], fire[21], fire[22], 
-        fire[23], fire[24], fire[25], fire[26], fire[27], fire[28], fire[29], fire[30], fire[31], fire[32], fire[33], fire[34],
-        fire[35], fire[36], fire[37], fire[38], fire[39], fire[40], fire[41], fire[42], fire[43], fire[44],fire[45], fire[46], 
-        fire[47], fire[48], fire[49], fire[50], fire[51], fire[52], fire[53], fire[54], fire[55], fire[56], fire[57], fire[58], fire[59])
+    fireAnim = loadAnimation(fire[0], fire[1], fire[2], fire[3], fire[4], fire[5])
 
     idleLeft = loadImage('Apollo/Idle/tile010.png');
     idleRight = loadImage('Apollo/Idle/tile030.png');
-    idle = loadImage('Apollo/Idle/tile000.png')
+    idle = loadImage('Apollo/Idle/tile000.png');
 }
 
 function setup() {
