0b8115fb73ef70e7e9c57cc34c5819fb4ba1c977
Atharva Mishra <atharvamishra956@gmail.com>
2020-12-13 19:55:51 +0530

    added sword and fire

--- sketch.js
+++ sketch.js
@@ -13,6 +13,16 @@ var apollo, left, right, lookingLeft, lookingRight, leftSteps, rightSteps, movin
 // fire 
 var fire1, fire2, fire, fireAnim;
 
+// health bar
+var healthInt, healthBar, healthBack;
+healthInt = 100;
+
+// sword 
+var swordImage, swordSprite, hasSword1, hasSword2;
+var leftSwordImg, rightSwordImg;
+var sword2;
+
+
 leftSteps = 0;
 rightSteps = 0;
 
@@ -46,6 +56,10 @@ function preload() {
     idleLeft = loadImage('Apollo/Idle/tile010.png');
     idleRight = loadImage('Apollo/Idle/tile030.png');
     idle = loadImage('Apollo/Idle/tile000.png');
+
+    swordImage = loadImage('Apollo/Sword/Sword.png');
+    leftSwordImg = loadImage('Apollo/Sword/SwordLeft.png');
+    rightSwordImg = loadImage('Apollo/Sword/SwordRight.png');
 }
 
 function setup() {
@@ -56,16 +70,24 @@ function setup() {
     invisGround= createSprite(windowWidth/2, windowHeight-100, windowWidth, 20);
     invisGround.visible = false;
 
-    fire1 = createSprite(windowWidth/1.75, windowHeight/1.75, 20, 20)
+    fire1 = createSprite(windowWidth/1.75, windowHeight/1.95, 20, 20)
     fire1.addAnimation('Fire', fireAnim)
     fire1.scale = 0.5;
-    fire1.visible = false;
+    
 
-    fire2 = createSprite(windowWidth/3.9, windowHeight/1.75, 20, 20)
+    fire2 = createSprite(windowWidth/3.9, windowHeight/1.95, 20, 20)
     fire2.addAnimation('Fire', fireAnim)
     fire2.scale = 0.5;
-    fire2.visible = false;
 
+    //healthBack = createSprite(windowWidth/0.5, windowHeight/)
+
+    swordSprite = createSprite(windowWidth/5, windowHeight-120)
+    swordSprite.addImage(swordImage)
+    swordSprite.scale = 0.25
+    swordSprite.visible = false;
+
+    sword2 = createSprite(100, 100)
+    sword2.visible = false;
 
     edges = createEdgeSprites();
 }
@@ -78,6 +100,9 @@ function draw() {
 
     moveLeft();
     moveRight();
+
+    getSword();
+    useSword();
 
     drawSprites();
 }
