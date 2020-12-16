76631fd61024d46ae42bc419c22ebcb98bb10bcb
Atharva Mishra <atharvamishra956@gmail.com>
2020-12-16 21:41:39 +0530

    added the rest of the scenes

--- Functions/Images/loadSceneImages.js
+++ Functions/Images/loadSceneImages.js
@@ -1,5 +1,7 @@
 function loadSceneImages() {
     scene1 = loadImage('Scenes/Cave.jpg');
     scene2 = loadImage('Scenes/Village.jpg');
-    scene3 = loadImage('Scenes/Cave2.jpg')
+    scene3 = loadImage('Scenes/Cave2.jpg');
+    scene4 = loadImage('Scenes/castle2.jpg');
+    scene5 = loadImage('Scenes/castle1.jpg')
 }
